69adad62baf914acf3a1da8af88964e037293a94
Abi14297 <abirami14297@gmail.com>
2026-06-29 14:18:23 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>🚀 React CI/CD Demo</h1>
+
+      <h2>Deployment Successful</h2>
+
+      <p>Welcome to GitHub Actions + AWS EC2 Demo</p>
+    </div>
+  );
+}
+
+export default App;
